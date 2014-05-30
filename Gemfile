cd7deedd579e697cb676b23301b4930bc69d61b7
GM <gusisonline@gmail.com>
2014-05-30 13:04:40 -0400

    Changed prouduction file to url

--- Gemfile
+++ Gemfile
@@ -24,6 +24,7 @@ gem 'sdoc', '~> 0.4.0',          group: :doc
 gem 'bootstrap-sass'
 gem 'devise', '~> 3.2.4'
 gem 'paperclip', '~>3.0'
+gem 'aws-sdk', '~> 1.20.0'
 #gem 'paperclip', github: 'thoughtbot/paperclip'
 group :development, :test do
 	gem 'sqlite3'	
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,6 +83,14 @@ Rails.application.configure do
 
   # Required for Heroku
   #Note this is for the public site and changing localhost for production to a public DNS host name required
-  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.default_url_options = { host: 'http://gidiup-pinteresting.herokuapp.com/' }
 
+config.paperclip_defaults = {
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => ENV['AWS_BUCKET'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  }
+}
 end
